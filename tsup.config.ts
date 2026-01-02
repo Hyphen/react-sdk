afb3a8a75dede7235ce1f769df956b3197eeb968
Christian Llontop <me@chrisllontop.com>
2026-01-02 15:43:31 -0500

    fix: add CommonJS support

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,10 +2,17 @@ import { defineConfig } from 'tsup';
 
 export default defineConfig({
   entry: ['src/index.ts'],
-  format: ['esm'],
+  format: ['esm', 'cjs'],
   dts: true,
   clean: true,
+  splitting: false,
+  target: 'es2020',
+  outExtension: ({ format }) => {
+    return format === 'esm' ? { js: '.js' } : { js: '.cjs' };
+  },
   esbuildOptions(options) {
     options.jsx = 'automatic';
   },
+  external: ['react', 'react-dom'],
+  noExternal: ['@hyphen/browser-sdk'],
 });
